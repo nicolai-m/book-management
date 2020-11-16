86a7a00fb4d3335905f8848fe9dd0083020d85de
nicolai@maliske.net <nicolai@maliske.net>
2020-11-16 19:15:14 +0100

    Datenbank-Tabelle für Bücher hinzugefügt

---
+++ src/Entity/Books.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BooksRepository::class)
+ */
+class Books
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $borrowed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getBorrowed(): ?bool
+    {
+        return $this->borrowed;
+    }
+
+    public function setBorrowed(bool $borrowed): self
+    {
+        $this->borrowed = $borrowed;
+
+        return $this;
+    }
+}
