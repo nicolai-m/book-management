21d70898a8b58923eb5454b873e11e5753f81213
nicolai@maliske.net <nicolai@maliske.net>
2020-11-17 11:13:57 +0100

    Anpassungen an der Datenbank

--- migrations/Version20201117075720.php
+++ migrations/Version20201117075720.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201116181232 extends AbstractMigration
+final class Version20201117075720 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20201116181232 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE books (id INT AUTO_INCREMENT NOT NULL, isbn VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, borrowed TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE books (id INT AUTO_INCREMENT NOT NULL, isbn BIGINT NOT NULL, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, borrowed TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
--- src/Entity/Books.php
+++ src/Entity/Books.php
@@ -18,7 +18,7 @@ class Books
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="bigint")
      */
     private $isbn;
 
